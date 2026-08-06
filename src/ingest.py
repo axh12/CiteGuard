fc2de08709abb4db8a6173abbee40e94577ea0db
Ashrita Hatwar T <ashritahatwar@gmail.com>
2026-08-07 01:37:59 +0530

    Add baseScore extraction and JSON pagination fix

--- src/ingest.py
+++ src/ingest.py
@@ -1,6 +1,8 @@
 import requests
+import json
 
 url="https://services.nvd.nist.gov/rest/json/cves/2.0?resultsPerPage=200"
+cve_records=[]
 response=requests.get(url)
 if response.status_code == 200:
     data=response.json()
@@ -8,6 +10,10 @@ if response.status_code == 200:
 
 for vuln in data["vulnerabilities"]:
     cvn_id=vuln["cve"]["id"]
+    base_score="10.0"
     for desc in vuln["cve"]["descriptions"]:
         if desc["lang"]=="en":
-            print("ID=",cvn_id,"Description=",desc["value"])
+            cve_records.append({"id": cvn_id, "description": desc["value"],"baseScore": base_score})
+
+with open("data/raw/cves.json", "w") as f:
+    json.dump(cve_records, f, indent=2)
